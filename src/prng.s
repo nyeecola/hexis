10e01d3834ed49637863ff6d56afcfe24ca4c253
lcagustini <torresmon235@gmail.com>
2019-02-15 16:26:09 -0200

    Optimized GCC generated xorshift

--- src/prng.s
+++ src/prng.s
@@ -1,42 +1,20 @@
-	.text
-	.thumb_func
-	.type	xorshift32, %function
+    .text
+    .thumb_func
+    .type	xorshift32, %function
 xorshift32:
-	@ Function supports interworking.
-	@ args = 0, pretend = 0, frame = 16
-	@ frame_needed = 1, uses_anonymous_args = 0
-	push	{r7, lr}
-	sub	sp, #16
-	mov	r7, sp
-	str	r0, [r7, #4]
-	ldr	r3, [r7, #4]
-	ldr	r3, [r3]
-	str	r3, [r7, #12]
-	ldr	r3, [r7, #12]
-	lsl	r3, #13
-	ldr	r2, [r7, #12]
-	eor	r3, r2
-	str	r3, [r7, #12]
-	ldr	r3, [r7, #12]
-	lsr	r3, #17
-	ldr	r2, [r7, #12]
-	eor	r3, r2
-	str	r3, [r7, #12]
-	ldr	r3, [r7, #12]
-	lsl	r3, #5
-	ldr	r2, [r7, #12]
-	eor	r3, r2
-	str	r3, [r7, #12]
-	ldr	r3, [r7, #4]
-	ldr	r2, [r7, #12]
-	str	r2, [r3]
-	ldr	r3, [r7, #12]
-	mov	r0, r3
-	mov	sp, r7
-	add	sp, #16
-	@ sp needed
-	pop	{r7}
-	pop	{r1}
-	bx	r1
-	.size	xorshift32, .-xorshift32
-	.ident	"GCC: (devkitARM release 50) 8.2.0"
+    push {r1-r3}
+    ldr	r1, [r0]
+    mov r3, r1
+    lsl	r3, #13
+    mov r2, r1
+    eor	r3, r2
+    lsr	r3, #17
+    mov r2, r1
+    eor	r3, r2
+    lsl	r3, #5
+    mov r2, r1
+    eor	r3, r2
+    str	r3, [r0]
+    mov	r0, r3
+    pop {r1-r3}
+    bx lr
